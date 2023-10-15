95632fd72f4e51b9f72edabd47cc15deecce9393
kang <1457857389@qq.com>
2023-10-15 19:03:56 +0800

    v2-版本更新

--- 14-golang-IM-System/server.go
+++ 14-golang-IM-System/server.go
@@ -3,38 +3,87 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
+/*
+ * 服务器类
+ */
 type Server struct {
 	Ip   string
 	Port int
+
+	// 在线用户的列表
+	OnlineMap map[string]*User
+	mapLock   sync.RWMutex
+
+	// 消息广播的channel
+	Message chan string
 }
 
 // 创建一个server接口
 func NewServer(ip string, port int) *Server {
 	server := &Server{
-		Ip:   ip,
-		Port: port,
+		Ip:        ip,
+		Port:      port,
+		OnlineMap: make(map[string]*User),
+		Message:   make(chan string),
 	}
 	return server
 }
 
-func (this *Server) Handler(conn net.Conn) {
+// 广播消息的方法
+func (serv *Server) BroadCast(user *User, msg string) {
+	sendMsg := "[" + user.Addr + "]" + user.Name + ":" + msg
+	serv.Message <- sendMsg
+}
+
+// 监听Message广播消息channel的goroutine，一旦Message有消息就发送给全部的在线User
+func (serv *Server) ListenMessage() {
+	for {
+		msg := <-serv.Message
+
+		// 将消息发送给全部的在线User
+		serv.mapLock.Lock()
+		for _, cli := range serv.OnlineMap {
+			cli.C <- msg
+		}
+		serv.mapLock.Unlock()
+	}
+}
+
+func (serv *Server) Handler(conn net.Conn) {
 	// ...当前连接的业务
-	fmt.Println("链接建立成功")
+	// fmt.Println("链接建立成功")
+
+	user := NewUser(conn)
+
+	// 用户上线，将用户加入到onlineMap中
+	serv.mapLock.Lock()
+	serv.OnlineMap[user.Name] = user // 多用户操作同一个map，互斥访问
+	serv.mapLock.Unlock()
+
+	// 广播当前用户上线消息
+	serv.BroadCast(user, "已上线")
+
+	// 当前handler阻塞
+	select {}
 }
 
 // 启动服务器接口
-func (this *Server) Start() { // Server类的成员方法
+func (serv *Server) Start() { // Server类的成员方法
 	// #1 socket listen
 	// net.Listen()的操作相当于socket()-bind()-listen()的融合。listener是监听fd
-	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", this.Ip, this.Port)) // address格式：127.0.0.1:8888
+	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", serv.Ip, serv.Port)) // address格式：127.0.0.1:8888
 	if err != nil {
 		fmt.Println("net.Listen error: ", err)
 		return
 	}
 	// close listen socket
 	defer listener.Close()
+
+	// 启动监听Message的goroutine
+	go serv.ListenMessage()
 
 	for {
 		// #2 accept
@@ -46,6 +95,6 @@ func (this *Server) Start() { // Server类的成员方法
 		}
 
 		// #3 do handler
-		go this.Handler(conn)	// 用一个协程来处理，主go程继续执行Accept()
+		go serv.Handler(conn) // 用一个协程goroutine来处理，主go程继续执行Accept()
 	}
 }
